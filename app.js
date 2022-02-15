fe72cdc10408b323d8d745efc12698808fd50f6c
chartataglance <mariadass@msn.com>
2022-02-15 19:41:29 +0000

    update

--- app.js
+++ app.js
@@ -12,3 +12,31 @@ function removeBorder(){
 }
 
 tabitems.forEach(item=> item.addEventListener('click',selectItem));
+
+const navSlide = () => {
+
+
+   const burger = document.querySelector('.burger')
+   const nav = document.querySelector('.nav-links')
+   const navLinks = document.querySelectorAll('.nav-links li')
+   const contentHide = document.querySelector('.content')
+   
+   burger.addEventListener('click' , () => {
+      nav.classList.toggle('nav-active')
+      contentHide.classList.toggle('dimm')
+      navLinks.forEach((link,index) =>{
+         if(link.style.animation){
+            link.style.animation = '';
+         } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/5}s`;
+           
+   
+         }
+      })
+      burger.classList.toggle('toggle');
+      
+   })
+   
+   }
+   
+   navSlide();
