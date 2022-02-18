cce5f2c3a266a67fe894c4aa39e492b029db0703
chartataglance <mariadass@msn.com>
2022-02-18 18:41:26 +0000

    all change

---
+++ main.js
@@ -0,0 +1,24 @@
+/** @format */
+
+const navSlide = () => {
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav-links");
+	const navLinks = document.querySelectorAll(" nav .nav-links li");
+   const contentHide = document.querySelector('.earth')
+	burger.addEventListener("click", () => {
+		nav.classList.toggle("nav-active");
+
+      navLinks.forEach((link,index) => {
+         if(link.style.animation){
+            link.style.animation = '';
+         } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/7}s`;
+         }
+      });
+      burger.classList.toggle('toggle');
+      contentHide.classList.toggle('dimm')
+	});
+	
+};
+
+navSlide();
